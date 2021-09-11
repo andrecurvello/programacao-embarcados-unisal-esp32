70d5fdbd92c8a8132c35a0a68a32c0a52a8dbd36
Andre Curvello <andre.ml.curvello@gmail.com>
2021-09-11 16:52:14 -0300

    Interrupcao de GPIO

--- src/main.c
+++ src/main.c
@@ -1,9 +1,4 @@
 // 1o - Bibliotecas de referencia para o desenvolvimento do codigo
-/* 
-Isso eh um 
-comentario multi-linha
-*/
-
 // Adicionando referencias para chamadas da biblioteca C (printf/scanf)
 #include <stdio.h>
 // Adicionando referencias para as configuracoes da SDK
@@ -20,7 +15,9 @@ comentario multi-linha
 
 // 2o - Defines para referencia de dados a serem usados no programa
 // Entenda o define como "apelido" - define apelido original
-#define LED_PLACA   GPIO_NUM_2
+#define LED_PLACA       GPIO_NUM_2
+#define BOTAO_1         GPIO_NUM_22
+#define LED_CONTROLE    GPIO_NUM_21
 
 // 3o - Variaveis globais (evitem se possivel, mas usem com cuidado)
 // uint32_t = unsigned int 32 bits - variavel inteira, sem sinal, com dimensao de 32 bits
@@ -28,7 +25,23 @@ uint32_t contador = 0;
 
 // 4o - Prototipos de funcoes presentes no codigo (quando nao usado .h a parte)
 
-// 5o - Implementacao dos metodos e tarefas
+// 5o - Implementacao dos metodos e tarefas -----------------------------------
+
+// Callback para tratamento da interrupcao do botao
+static void IRAM_ATTR gpio_isr_handler(void *arg)
+{
+    // Verifica se o botao 1 foi a fonte da interrupcao
+    // 1 - Como fazer debounce na leitura da interrupcao de gpio?
+    if (BOTAO_1 == (uint32_t) arg) //typecast para tipo uint32_t - inteiro usado na definicao dos gpios
+    {
+        if (gpio_get_level((uint32_t) arg) == 0) 
+        {
+            contador++;
+        }
+    }
+}
+
+
 void app_main(void) {
     // Exibe a mensagem no terminal
     printf("Inicializando Esquenta ESP32... \n");
@@ -52,27 +65,41 @@ void app_main(void) {
         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embarcada" : "externa");
 
     // Vamos configurar o uso do LED (GPIO 2)
-    gpio_reset_pin(LED_PLACA);
-    gpio_set_direction(LED_PLACA, GPIO_MODE_OUTPUT);
+    // "Metodo discreto" de configuracao de GPIOs
+    //gpio_reset_pin(LED_PLACA);
+    //gpio_set_direction(LED_PLACA, GPIO_MODE_OUTPUT);
 
-    // Tudo que foi feito ate aqui sera executado uma unica vez quando o microcontrolador iniciar
+    // Metodo de configuracao de pinos usando o gpio_config_t
+    // Primeiramente configuramos o botao
+    gpio_config_t button_config = {
+        .intr_type = GPIO_INTR_NEGEDGE, // interrupcao em borda negativa 1->0
+        .mode = GPIO_MODE_INPUT,
+        .pin_bit_mask = (1ULL << BOTAO_1),
+        .pull_down_en = 0,
+        .pull_up_en = 1
+    };
+    // configura o gpio
+    gpio_config(&button_config);
 
-    // Laco de execucao continua - ficara em execucao ate expressamente usarmos break, ou chip desligar
-    while(1) {
-        printf("----------------------- \n");
-        printf("Contador: %d \n", contador);
-        // desliga led?
-        printf("Desligando LED...\n");
-        gpio_set_level(LED_PLACA, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // espera 1000ms = 1s
-        // liga led?
-        printf("Ligando LED...\n");
-        gpio_set_level(LED_PLACA, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        contador++; // incrementa o contador a cada... 2 segundos! - contador++ -> contador = contador+1
-        if (contador == 4294967295) {
-            contador = 0;
-            // avisar / notificar / tomar alguma acao
-        }
+    // Vamos configurar "os leds"
+    gpio_config_t led_config = {
+        .intr_type = GPIO_INTR_DISABLE, //sem interrupcao
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = (1ULL << LED_PLACA) | (1ULL << LED_CONTROLE),
+        .pull_down_en = 0
+    };
+    gpio_config(&led_config);
+
+    gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1); // Estamos configurando uma rotina de tratamento de interrupcao de baixa prioridade
+    gpio_isr_handler_add(BOTAO_1, gpio_isr_handler, (void*) BOTAO_1);
+
+    uint8_t estado_led = 0;
+
+    while(1) 
+    {
+        vTaskDelay( 1000 / portTICK_PERIOD_MS ); // delay de 30ms na API do FreeRTOS, pra debounce de tecla
+        printf("Contador: %d\n", contador);
+        gpio_set_level(LED_PLACA, estado_led);
+        estado_led = !estado_led; // 0->1, 1->0, 1->0...
     }
 }
